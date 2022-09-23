1ecf41b420ebc09d83adf4b452cdfaf3017b6dea
Nouss <noussmail@protonmail.com>
2022-09-23 22:10:38 +0200

    Commit

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,9 +1,23 @@
-const call = function () {
+const call = async function () {
+  const sObj = {
+    Saga1: "East Blue",
+    Saga2: "Alabasta",
+    Saga3: "Skypiea",
+    Saga4: "Water Seven",
+    Saga5: "Thriller Bark",
+    Saga6: "MarinFord (❤)",
+    Saga7: "Île des hommes poissons",
+    Saga8: "DressRosa",
+    Saga9: "Quatres Empereurs",
+  };
+
   const card = document.getElementsByClassName("saga");
 
   let i = 9;
 
   for (const e of card) {
+    console.log(`Saga: ${i} - ${sObj[`Saga${i}`]}`);
+
     e.innerHTML = `<a href="../Pages/blank.html"><img src="../Assets/Saga/Saga${i}.jpeg" /></a>`;
     i--;
   }
