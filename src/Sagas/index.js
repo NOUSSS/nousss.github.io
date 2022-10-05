c768057c6a61a425e734c4ba7a6551a131240fac
Nouss <noussmail@protonmail.com>
2022-10-05 22:15:59 +0200

    Commit

--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -141,21 +141,21 @@ function getSagas() {
 const obj = {
   1: {
     name: "East Blue",
-    aliases: ["shanks", "debut", "début", "start", "luffy", "log town", "roger", "baggy"],
+    aliases: ["shanks", "debut", "début", "start", "luffy", "log town", "roger", "baggy", "1"],
   },
-  2: { name: "Alabasta", aliases: ["crocodile"] },
-  3: { name: "Les Îles célestes", aliases: ["ile", "poneglyphe"] },
-  4: { name: "Water Seven", aliases: ["robin", "cp9"] },
-  5: { name: "Thriller Bark", aliases: ["moria"] },
-  6: { name: "Guerre au sommet (❤️)", aliases: ["marinford", "sabaody", "boa hancok"] },
-  7: { name: "Île des hommes poissons", aliases: ["ile"] },
-  8: { name: "Dressrosa", aliases: ["doflamingo", "vegapunk", "quatre", "4 empereurs"] },
+  2: { name: "Alabasta", aliases: ["crocodile", "2"] },
+  3: { name: "Les Îles célestes", aliases: ["ile", "poneglyphe", "3"] },
+  4: { name: "Water Seven", aliases: ["robin", "cp9", "4"] },
+  5: { name: "Thriller Bark", aliases: ["moria", "5"] },
+  6: { name: "Guerre au sommet (❤️)", aliases: ["marinford", "sabaody", "boa hancok", "6"] },
+  7: { name: "Île des hommes poissons", aliases: ["ile", "7"] },
+  8: { name: "Dressrosa", aliases: ["doflamingo", "vegapunk", "quatre", "4 empereurs", "8"] },
   9: {
     name: "Whole Cake Island",
-    aliases: ["tougato", "big mom", "sanji", "vinsmoke", "judge", "quatre", "4 empereurs"],
+    aliases: ["tougato", "big mom", "sanji", "vinsmoke", "judge", "quatre", "4 empereurs", "9"],
   },
   10: {
     name: "Pays de Wano",
-    aliases: ["kaido", "wanocuni", "zoro", "ashura", "quatre", "4 empereurs"],
+    aliases: ["kaido", "wanocuni", "zoro", "ashura", "quatre", "4 empereurs", "10"],
   },
 };
