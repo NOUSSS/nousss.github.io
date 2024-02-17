4fd803bd1cce3c7f983787740519cb4762c4706b
Nouss Github <magicnouss@gmail.com>
2024-02-17 22:03:15 +0100

    added environment variables

--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -1,10 +1,4 @@
-declare global {
-  namespace Window {
-    interface EPS {
-      eps1: string[];
-      epsAS: string[];
-    }
-  }
+interface Window {
+  eps1: string[];
+  epsAS: string[];
 }
-
-export {};
