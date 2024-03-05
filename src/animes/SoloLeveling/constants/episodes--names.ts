873b4353cc12b15b780338b780a06defdd213b7d
Nouss Github <magicnouss@gmail.com>
2024-03-05 23:11:44 +0100

    ajout du nom du nouvel episode de solo leveling

--- src/animes/SoloLeveling/constants/episodes--names.ts
+++ src/animes/SoloLeveling/constants/episodes--names.ts
@@ -1,35 +1,39 @@
 const episodes = [
   {
     name: "I'm Used to It",
-    index: '1',
+    index: "1",
   },
   {
-    name: 'If I Had One More Chance',
-    index: '2',
+    name: "If I Had One More Chance",
+    index: "2",
   },
   {
     name: "It's Like a Game",
-    index: '3',
+    index: "3",
   },
   {
     name: "I've Gotta Get Stronger",
-    index: '4',
+    index: "4",
   },
   {
-    name: 'A Pretty Good Deal',
-    index: '5',
+    name: "A Pretty Good Deal",
+    index: "5",
   },
   {
-    name: 'The Real Hunt Begins',
-    index: '6',
+    name: "The Real Hunt Begins",
+    index: "6",
   },
   {
     name: "Let's See How Far I Can Go",
-    index: '7',
+    index: "7",
   },
   {
-    name: 'This Is Frustrating',
-    index: '8',
+    name: "This Is Frustrating",
+    index: "8",
+  },
+  {
+    name: "You've Been Hiding Your Skills",
+    index: "9",
   },
 ];
 
