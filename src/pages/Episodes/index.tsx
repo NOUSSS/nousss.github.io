b44cd2fe2c05aac72889f6f559c165da28fce473
Nouss Github <magicnouss@gmail.com>
2024-03-23 00:49:08 +0100

    amélioration du toggle switch

--- src/pages/Episodes/index.tsx
+++ src/pages/Episodes/index.tsx
@@ -604,7 +604,7 @@ const Episodes = () => {
       />
 
       {isMobile() ? null : (
-        <label className="hideEpisodesNames relative -left-5 flex cursor-pointer transition-all duration-300 ease-out before:absolute before:-right-12 before:bottom-2 before:h-[10px] before:w-10 before:rounded-full before:bg-[#3c3c3c] before:transition-all before:duration-200 has-[:checked]:before:bg-main has-[:checked]:before:brightness-[.55]">
+        <label className="hideEpisodesNames relative -left-5 flex transition-all duration-300 ease-out before:absolute before:-right-12 before:h-7 before:w-12 before:rounded-full before:border before:border-neutral-700 before:border-opacity-50 before:bg-transparent before:transition-all before:duration-200 hover:before:border-opacity-100 has-[:checked]:before:bg-main has-[:checked]:before:brightness-50">
           <p>Cacher le nom des épisodes</p>
 
           <input
@@ -629,7 +629,8 @@ const Episodes = () => {
                 }
             }}
           />
-          <span className="relative left-6 h-6 w-6 rounded-full bg-main transition-all duration-200 peer-checked:left-[52px]"></span>
+
+          <span className="relative left-6 top-1 h-5 w-5 rounded-full bg-main transition-all duration-200 peer-checked:left-11"></span>
         </label>
       )}
 
