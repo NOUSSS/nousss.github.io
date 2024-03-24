7f6e1fa591b296f34265cb307cc495e96dc77fc3
Nouss Github <magicnouss@gmail.com>
2024-03-24 22:32:04 +0100

    reduce height of items

--- src/app/ui/Select.tsx
+++ src/app/ui/Select.tsx
@@ -116,7 +116,7 @@ export default function Select({
 
       <div
         ref={menuRef}
-        className="absolute left-0 top-14 z-10 hidden max-h-64 w-full animate-appear overflow-y-auto rounded-md bg-white p-2 text-black shadow-xl transition-all duration-200 ease-in-out"
+        className="absolute left-0 top-14 z-10 hidden max-h-64 w-full animate-appear overflow-auto rounded-md bg-white p-2 text-black shadow-xl transition-all duration-200 ease-in-out"
       >
         {items.map((item, index) => (
           <ul key={index}>
@@ -127,7 +127,7 @@ export default function Select({
                   appear();
                 }
               }}
-              className={`flex h-9 cursor-default items-center justify-center rounded-md text-base transition-colors ${item.disabled ? "opacity-50 hover:bg-transparent" : "hover:bg-[#3e63dd] hover:text-white"} `}
+              className={`flex h-8 cursor-default items-center justify-center rounded-md text-base transition-colors ${item.disabled ? "opacity-50 hover:bg-transparent" : "hover:bg-[#3e63dd] hover:text-white"} `}
             >
               <p>{item.name}</p>
             </li>
