f0c3017fe2e37fc4f89874ba0537aa055841f639
Nouss Github <magicnouss@gmail.com>
2024-03-30 18:05:09 +0100

    Ajout du nom des episodes de air gear

--- src/animes/AirGear/air-gear.ts
+++ src/animes/AirGear/air-gear.ts
@@ -2,6 +2,7 @@ import { getImage } from "./constants/image-saisons";
 import { Anime } from "@/app/class/anime";
 
 import Affiche from "@/assets/Animes/AirGear/Affiche.jpg";
+import names from "./constants/episodes-names";
 
 export default class AirGear extends Anime {
   constructor() {
@@ -21,9 +22,12 @@ export default class AirGear extends Anime {
         `https://anime-sama.fr/catalogue/air-gear/saison${index}/${lang}/episodes.js`,
 
       horsSeries: [{ saison: "1", hs: [21] }],
+
       allIndex: {
         1: 0,
       },
+
+      names,
     };
   }
 }
