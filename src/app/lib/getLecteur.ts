8d09618582727b1ca31d31a1f465a4b2e71dc062
Nouss Github <magicnouss@gmail.com>
2024-04-02 17:54:24 +0200

    priorité sur le lecteur sibnet en deuxieme et vidmoly en premier

--- src/app/lib/getLecteur.ts
+++ src/app/lib/getLecteur.ts
@@ -4,31 +4,34 @@ interface WindowKeys {
   [key: string]: string[];
 }
 
-const containsMyvi = (episodes?: string[]): boolean =>
-  episodes?.[0].includes("myvi") ?? false;
-
-const containsVidmoly = (episodes?: string[]): boolean =>
-  episodes?.some((episode) => episode.includes("vidmoly")) ?? false;
+const contains = (episodes: string[], query: string): boolean =>
+  episodes?.[0].includes(query) ?? false;
 
 export const getLecteur = (): LecteurReturnType => {
   const eps = ["eps1", "eps2", "eps3", "eps4"];
 
-  const { vidmolyEps, otherEps } = eps.reduce(
+  const { vidmolyEps, sibnetEps, otherEps } = eps.reduce(
     (acc, cur) => {
       const value = (window as unknown as WindowKeys)[cur];
 
-      if (value && containsVidmoly(value)) {
+      if (value && contains(value, "vidmoly")) {
         acc.vidmolyEps[cur] = value;
-      } else if (value && !containsMyvi(value)) {
+      } else if (value && contains(value, "sibnet")) {
+        acc.sibnetEps[cur] = value;
+      } else if (value && !contains(value, "myvi")) {
         acc.otherEps[cur] = value;
       }
 
       return acc;
     },
-    { vidmolyEps: {} as LecteurReturnType, otherEps: {} as LecteurReturnType },
+    {
+      vidmolyEps: {} as LecteurReturnType,
+      sibnetEps: {} as LecteurReturnType,
+      otherEps: {} as LecteurReturnType,
+    },
   );
 
-  const orderedEps = { ...vidmolyEps, ...otherEps };
+  const orderedEps = { ...vidmolyEps, ...sibnetEps, ...otherEps };
 
   return orderedEps;
 };
