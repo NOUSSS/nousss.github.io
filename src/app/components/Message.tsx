b5a66ea0efe49f1e0fc33c853a7497834f197750
Nouss Github <magicnouss@gmail.com>
2024-04-06 19:20:03 +0200

    icon red and reduced

--- src/app/components/Message.tsx
+++ src/app/components/Message.tsx
@@ -15,9 +15,9 @@ export default function Message({ message }: MessageProps) {
         <div className="relative mx-8 mt-12 rounded-md border border-neutral-700 p-3 text-left text-lg max-md:text-sm">
           <div
             onClick={() => setHidden(true)}
-            className="absolute -right-4 -top-4 m-1 cursor-pointer rounded-full bg-zinc-900 p-1"
+            className="absolute -right-4 -top-4 m-1 cursor-pointer rounded-full bg-red-500 p-1"
           >
-            <Remove />
+            <Remove width={20} height={20} />
           </div>
 
           {message}
