746c8920f766ade2193a2454a2dbadac842e537d
Nouss Github <magicnouss@gmail.com>
2024-04-13 20:44:32 +0200

    mx-0 on main sm instead of md and border

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,7 +39,7 @@ const Home = () => {
   }, [query.anime]);
 
   return (
-    <main className="top-24 mx-12 max-md:mx-0">
+    <main className="top-24 mx-12 max-sm:mx-0">
       <Head>
         {anime?.anime && (
           <title>{anime.anime} - Accueil - Mugiwara-no Streaming</title>
@@ -48,13 +48,13 @@ const Home = () => {
 
       <Title link={{ pathname: "/" }} />
 
-      <div className="relative my-12 flex h-auto rounded-md bg-zinc-900 bg-opacity-50 text-left max-xl:flex-col max-md:rounded-none">
+      <div className="relative my-12 flex h-auto rounded-md bg-zinc-900 bg-opacity-50 text-left max-xl:flex-col max-sm:rounded-none">
         {anime?.options.affiche && (
           <div>
             <Image
               alt={`affiche de ${anime.anime}`}
               src={anime.options.affiche!}
-              className="w-full rounded-l-md max-xl:rounded-l-none max-xl:rounded-t-md max-md:rounded-none md:w-[654px] md:max-w-[654px]"
+              className="w-full rounded-l-md max-xl:rounded-l-none max-xl:rounded-t-md max-sm:rounded-none md:w-[654px] md:max-w-[654px]"
             />
 
             <Image
