c7b6f0e913a05302bc174a8ea74820c18d9374fa
Nouss Github <magicnouss@gmail.com>
2024-04-19 01:59:47 +0200

    amélioration de l'embed

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -19,12 +19,16 @@ export default function Report() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          content: `Signalement : ${author}`,
           embeds: [
             {
               description: message,
               color: 0x04fbb7,
               timestamp: new Date().toISOString(),
+              author: {
+                name: author,
+                icon_url:
+                  "https://media.discordapp.net/attachments/1216077853838278758/1230668929337266299/Logo.png?ex=663428c1&is=6621b3c1&hm=c7859dfba42dc5d419c386bdac3fdc4aaafb3d399e5f7fcf566890bafc8bdf36&=&format=webp&quality=lossless&width=671&height=671",
+              },
             },
           ],
         }),
