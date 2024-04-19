08e98cd7d64762330b946a115c5b4eda82786e70
Nouss Github <magicnouss@gmail.com>
2024-04-19 22:37:33 +0200

    google tag manager

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -71,19 +71,37 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
   return (
     <>
-      {/* Google Tag Manager */}
+      {/* Google Tag Manager - Global base code */}
       <Script
-        id="google tag manager"
+        id="gtm-base"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KVG6T32C');`,
+          __html: `
+            (function(w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+              });
+              var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-KVG6T32C');
+          `,
         }}
       />
-      {/* End Google Tag Manager */}
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KVG6T32C"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
 
       <Head>
         <meta charSet="UTF-8" />
@@ -99,15 +117,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
         <title>Mugiwara-no Streaming</title>
       </Head>
-
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-KVG6T32C"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
 
       <Image
         className="fixed left-0 top-0 h-full w-full opacity-5 blur-2xl"
