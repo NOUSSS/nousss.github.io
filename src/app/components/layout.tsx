5f2bf62ce3b1120fb73182cc1aad5ba665333eed
Nouss Github <magicnouss@gmail.com>
2024-04-20 00:17:37 +0200

    removed old google tag manager

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -71,34 +71,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
   return (
     <>
-      {/* Google Tag Manager - Global base code */}
-      <Script
-        id="gtm-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-              });
-              var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s),
-                  dl = l != 'dataLayer' ? '&l=' + l : '';
-              j.async = true;
-              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-KVG6T32C');
-          `,
-        }}
-      />
-
       {/* Google Analytics (gtag.js) */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-F9N37DQQDM"
         strategy="afterInteractive"
       />
+
       <Script
         id="gtag-init"
         strategy="afterInteractive"
@@ -111,15 +89,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           `,
         }}
       />
-
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-KVG6T32C"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
 
       <Head>
         <meta charSet="UTF-8" />
