6974cc3a1ee9378b9395ebbaf524e42cfecc4f40
Nouss Github <magicnouss@gmail.com>
2024-04-21 17:51:01 +0200

    optimisatgions des conditions

--- src/app/utils/Saisons/getSaisons.tsx
+++ src/app/utils/Saisons/getSaisons.tsx
@@ -9,43 +9,41 @@ export function getSaisons() {
 
   const saisonsList = [];
 
-  if (options && options.EPISODES_OPTIONS) {
+  if (options && options.EPISODES_OPTIONS && options.saisons) {
     const { allIndex } = options.EPISODES_OPTIONS;
     const { saisons } = options;
 
-    if (saisons && allIndex) {
-      for (let i = 0; i < Object.keys(saisons).length; i++) {
-        const intervalEpisodes = `${allIndex[i + 1] + 1} - ${allIndex[i + 2]}`;
-        const saisonsValues = Object.values(saisons!);
+    for (let i = 0; i < Object.keys(saisons).length; i++) {
+      const intervalEpisodes = `${allIndex[i + 1] + 1} - ${allIndex[i + 2]}`;
+      const saisonsValues = Object.values(saisons!);
 
-        saisonsList.push({
-          id: `${saisonsValues[i].name}|${
-            typeof saisonsValues[i].aliases === "undefined"
-              ? ""
-              : saisonsValues[i].aliases?.join(", ")
-          } ${i === Object.keys(saisonsValues).length - 1 ? "" : intervalEpisodes}`,
-          element: (
-            <>
-              <div className="flex-col overflow-hidden rounded-md text-center">
-                <Image
-                  src={saisonsValues?.[i].image()!}
-                  id={i.toString()}
-                  alt="poster de saison"
-                  className="h-36 min-h-36 w-24 min-w-24 rounded-md transition-transform group-hover:scale-105 md:h-48 md:max-h-48 md:w-32 md:min-w-32"
-                />
-              </div>
+      saisonsList.push({
+        id: `${saisonsValues[i].name}|${
+          typeof saisonsValues[i].aliases === "undefined"
+            ? ""
+            : saisonsValues[i].aliases?.join(", ")
+        } ${i === Object.keys(saisonsValues).length - 1 ? "" : intervalEpisodes}`,
+        element: (
+          <>
+            <div className="flex-col overflow-hidden rounded-md text-center">
+              <Image
+                src={saisonsValues?.[i].image()!}
+                id={i.toString()}
+                alt="poster de saison"
+                className="h-36 min-h-36 w-24 min-w-24 rounded-md transition-transform group-hover:scale-105 md:h-48 md:max-h-48 md:w-32 md:min-w-32"
+              />
+            </div>
 
-              <p className="text-sm md:text-base">
-                <span>{saisonsValues?.[i].name}</span>
-                <br />
-                {i === Object.keys(saisonsValues).length - 1 && !allIndex[i + 2]
-                  ? ""
-                  : `(${intervalEpisodes})`}
-              </p>
-            </>
-          ),
-        });
-      }
+            <p className="text-sm md:text-base">
+              <span>{saisonsValues?.[i].name}</span>
+              <br />
+              {i === Object.keys(saisonsValues).length - 1 && !allIndex[i + 2]
+                ? ""
+                : `(${intervalEpisodes})`}
+            </p>
+          </>
+        ),
+      });
     }
   }
 
