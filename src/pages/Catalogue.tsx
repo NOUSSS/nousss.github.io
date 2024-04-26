634d9df57f0395f5abdc469993b848d3c892a6c6
NouSs <magicnouss@gmail.com>
2024-04-26 21:56:06 +0200

    Import style

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -1,10 +1,11 @@
 import { ANIMES } from "@/animes/constants";
-import Select from "@/app/components/Select";
 import { getAnime } from "@/app/lib/getAnime";
 import { getWallpaper } from "@/app/lib/getWallpaper";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef, useState } from "react";
+
+import Image from "next/image";
+import Select from "@/app/components/Select";
 
 export default function Catalogue() {
   const router = useRouter();
