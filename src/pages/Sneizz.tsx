767b3a6b571dd6939029f28628d04c0259e9f113
Nouss Github <magicnouss@gmail.com>
2024-05-04 21:49:18 +0200

    rounded aim

--- src/pages/Sneizz.tsx
+++ src/pages/Sneizz.tsx
@@ -2,29 +2,19 @@ import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 
 export default function Aim() {
-  const refs = useRef<HTMLDivElement[]>([]);
-  const [currentAim, setCurrentAim] = useState<number>();
+  const refs = useRef<(HTMLDivElement | HTMLButtonElement)[]>([]);
+
+  const [current, setCurrent] = useState<number>();
   const [score, setScore] = useState(0);
 
   function randomAim() {
-    refs.current.forEach((e) => {
-      e.classList.add("bg-transparent");
-      e.classList.remove("bg-sky-600");
-    });
-
     const random = Math.floor(Math.random() * refs.current.length);
-    const element = refs.current[random];
 
-    setCurrentAim(random);
-
-    element.classList.add("bg-sky-600");
-    element.classList.remove("bg-transparent");
+    setCurrent(random);
   }
 
   useEffect(() => {
-    if (refs.current) {
-      randomAim();
-    }
+    if (refs.current) randomAim();
   }, [refs.current]);
 
   return (
@@ -48,19 +38,30 @@ export default function Aim() {
         </p>
 
         <div className="w-full border border-neutral-700 md:w-[525px]">
-          {Array.from({ length: 40 }, (_, i) => (
-            <div
-              key={i}
-              ref={(el) => (refs.current[i] = el!)}
-              onClick={() => {
-                if (currentAim === i) {
-                  randomAim();
-                  setScore((state) => state + 1);
-                }
-              }}
-              className="inline-flex size-16 bg-transparent"
-            ></div>
-          ))}
+          {Array.from({ length: 40 }, (_, i) => {
+            if (current === i)
+              return (
+                <button
+                  key={i}
+                  ref={(el) => (refs.current[i] = el!)}
+                  onClick={() => {
+                    if (current === i) {
+                      randomAim();
+                      setScore((state) => state + 1);
+                    }
+                  }}
+                  className="inline-flex size-16 rounded-lg bg-sky-600"
+                ></button>
+              );
+            else
+              return (
+                <div
+                  key={i}
+                  ref={(el) => (refs.current[i] = el!)}
+                  className="inline-flex size-16 bg-transparent"
+                ></div>
+              );
+          })}
         </div>
       </main>
     </>
