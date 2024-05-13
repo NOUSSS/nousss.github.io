ee4e891b104b6756a51db2f8cca6af43f7be71ea
Nouss Github <magicnouss@gmail.com>
2024-05-14 01:14:06 +0200

    fix du Switch sur mobile

--- src/app/components/Switch.tsx
+++ src/app/components/Switch.tsx
@@ -7,17 +7,17 @@ interface SwitchProps {
 
 const Switch: FC<SwitchProps> = ({ placeholder, onChange }) => {
   return (
-    <label className="relative flex gap-8">
+    <label className="flex gap-8">
       {placeholder && <p>{placeholder}</p>}
 
-      <div className="relative transition-colors before:absolute before:-right-12 before:h-7 before:w-14 before:rounded-full before:border before:border-neutral-700 before:border-opacity-50 before:bg-transparent before:transition-all before:hover:border-opacity-100 before:has-[:checked]:bg-main before:has-[:checked]:brightness-50">
+      <div className="relative transition-colors before:absolute before:left-0 before:top-0 before:h-7 before:w-14 before:rounded-full before:border before:border-neutral-700 before:border-opacity-50 before:bg-transparent before:transition-all before:hover:border-opacity-100 before:has-[:checked]:bg-main before:has-[:checked]:brightness-50">
         <input
           type="checkbox"
           className="peer appearance-none"
           onChange={onChange}
         />
 
-        <span className="absolute -left-1 top-1 z-50 h-5 w-5 rounded-full bg-main transition-all peer-checked:left-6"></span>
+        <span className="absolute left-1 top-1 z-50 h-5 w-5 rounded-full bg-main transition-all peer-checked:left-8"></span>
       </div>
     </label>
   );
