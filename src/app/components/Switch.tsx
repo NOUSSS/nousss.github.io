ab00c4457df5847416533a54353fa750c09e3647
Nouss Github <magicnouss@gmail.com>
2024-05-16 20:33:55 +0200

    amélioration du Toggle Switch

--- src/app/components/Switch.tsx
+++ src/app/components/Switch.tsx
@@ -7,7 +7,7 @@ interface SwitchProps {
 
 const Switch: FC<SwitchProps> = ({ placeholder, onChange }) => {
   return (
-    <label className="flex gap-8">
+    <label className="group flex gap-8">
       {placeholder && <p>{placeholder}</p>}
 
       <div className="relative transition-colors before:absolute before:left-0 before:top-0 before:h-7 before:w-14 before:rounded-full before:border before:border-neutral-700 before:border-opacity-50 before:bg-transparent before:transition-all before:hover:border-opacity-100 before:has-[:checked]:bg-main before:has-[:checked]:brightness-50">
@@ -17,7 +17,9 @@ const Switch: FC<SwitchProps> = ({ placeholder, onChange }) => {
           onChange={onChange}
         />
 
-        <span className="absolute left-1 top-1 z-50 h-5 w-5 rounded-full bg-main transition-all peer-checked:left-8"></span>
+        <div className="absolute -left-1 top-1 z-50 h-5 w-5 peer-checked:left-10 peer-checked:rotate-180">
+          <span className="absolute z-50 h-5 w-5 scale-95 rounded-full bg-main transition-all group-hover:scale-100 group-active:w-6"></span>
+        </div>
       </div>
     </label>
   );
