9c15d43128aec9e8e4b8b487a4d7de37dc72b3b7
Nouss Github <magicnouss@gmail.com>
2024-06-20 19:45:37 +0200

    Ajout du nom des nouveaux episodes de Viral Hit

--- src/animes/ViralHit/constants/episodes-names.ts
+++ src/animes/ViralHit/constants/episodes-names.ts
@@ -11,6 +11,38 @@ const names = [
     name: "Rencard",
     index: 3,
   },
+  {
+    name: "Humiliation",
+    index: 4,
+  },
+  {
+    name: "Ami",
+    index: 5,
+  },
+  {
+    name: "Démon",
+    index: 6,
+  },
+  {
+    name: "Entraînement",
+    index: 7,
+  },
+  {
+    name: "Combat réel",
+    index: 8,
+  },
+  {
+    name: "Camarades",
+    index: 9,
+  },
+  {
+    name: "Nouveaux adversaires",
+    index: 10,
+  },
+  {
+    name: "La poitrine serrée",
+    index: 11,
+  },
 ];
 
 export default names;
