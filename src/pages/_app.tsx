155d682784a8eac9c6a2ff5e44fbc10005ed39b2
Nouss Github <magicnouss@gmail.com>
2024-10-07 19:59:11 +0200

    Ajout d'un Head dans le _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/assets/global.css";
 
 import RootLayout from "@/app/components/layout";
+import Head from "next/head";
 
 import { AppProps } from "next/app";
 import { Toaster } from "sonner";
@@ -9,6 +10,17 @@ import { FC } from "react";
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <RootLayout>
+      <Head>
+        <title>Mugiwara-no Streaming</title>
+
+        <meta
+          name="description"
+          content="Site de référencement d'animes (films + scans)"
+        />
+
+        <meta name="keywords" content="anime, scans, film, streaming" />
+      </Head>
+
       <Toaster
         position="top-center"
         theme="system"
