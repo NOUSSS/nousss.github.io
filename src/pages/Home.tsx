ffd41377039e915d8b15b8f8c4e2e1d53cc189fd
Nouss Github <magicnouss@gmail.com>
2024-10-14 22:23:17 +0200

    optimisation des proportions de l'image dans le Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -81,7 +81,7 @@ const Home = () => {
       <main className="top-0 flex flex-col gap-12 bg-zinc-900 bg-opacity-50 text-left min-[460px]:top-24">
         <div className="relative flex h-auto flex-col">
           {anime?.options.affiche && (
-            <div className="relative hidden justify-center overflow-hidden min-[460px]:flex md:h-[calc(100vh-28.75rem)]">
+            <div className="relative hidden justify-center overflow-hidden min-[460px]:flex lg:h-[calc(100vh-28.75rem)]">
               <Image
                 alt={`affiche de ${anime.anime}`}
                 src={anime.options.affiche!}
