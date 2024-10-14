13b823be471a62ed133471d99b73c888fa410767
Nouss Github <magicnouss@gmail.com>
2024-10-14 18:05:51 +0200

    fix film Hajime

--- src/animes/HajimeNoIppo/constants/films-names.ts
+++ src/animes/HajimeNoIppo/constants/films-names.ts
@@ -4,7 +4,7 @@ import { getImage } from "./images-films";
 export const films: Options.Film = {
   0: { name: "Champion Road", aliases: ["1"], image: () => getImage(0) },
   1: {
-    name: "Mashiba VF Kimura",
+    name: "Mashiba vs Kimura",
     aliases: ["2"],
     image: () => getImage(1),
   },
