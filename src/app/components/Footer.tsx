d7b2868355545c7415625a30a98558d0ff179443
Nouss Github <magicnouss@gmail.com>
2024-10-22 14:42:01 +0200

    Ajout du lien du serveur discord dans le Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,10 +4,21 @@ import Link from "next/link";
 const Footer = () => {
   const Copyright = icons["Copyright"];
   const Qui = icons["CircleUserRound"];
+  const Discord = icons["AtSign"];
 
   return (
     <footer className="mt-40 w-full border-t border-neutral-700 bg-zinc-900 bg-opacity-50 p-8">
       <div className="flex flex-col border-b border-neutral-700 pb-6 text-base">
+        <span className="inline-block">
+          <Link
+            target="_blank"
+            className="flex items-center justify-center gap-2 text-2xl text-main hover:underline"
+            href="https://discord.gg/q5qSRQypJU"
+          >
+            <Discord /> Serveur Discord
+          </Link>
+        </span>
+
         <span className="inline-block">
           <Link
             className="text-main hover:underline"
